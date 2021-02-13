beedd3c1aa12ee91dee87266e841699ba6b7ac5c
Aruna <aruna.snafu@gmail.com>
2021-02-13 23:43:28 +0000

    added sout(Helloworld3)

--- src/com/training/Snafu/Main.java
+++ src/com/training/Snafu/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("HelloWorld1");
         System.out.println("HelloWorld2");
         System.out.println("HelloWorld3");
+        System.out.println("HelloWorld5");
     }
 }
