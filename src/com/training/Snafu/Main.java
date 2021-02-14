dcd73fa117b8dec8dc491e98a8b69b7becf124fa
Aruna <aruna.snafu@gmail.com>
2021-02-14 14:08:14 +0000

    added 3.4,7 on Branch

--- src/com/training/Snafu/Main.java
+++ src/com/training/Snafu/Main.java
@@ -6,7 +6,10 @@ public class Main {
 	// write your code here
         System.out.println("HelloWorld1");
         System.out.println("HelloWorld2");
+        System.out.println("HelloWorld3");  //aded
+        System.out.println("HelloWorld4");  //addd
         System.out.println("HelloWorld5");
         System.out.println("HelloWorld6");
+        System.out.println("HelloWorld7"); //added
     }
 }
