978648fd5ffb7eb7367286c1bfb31e207ef5ab6a
Aruna <aruna.snafu@gmail.com>
2021-02-14 21:26:10 +0000

    added Test1 in Branch 1

--- src/com/training/Snafu/Main.java
+++ src/com/training/Snafu/Main.java
@@ -13,6 +13,6 @@ public class Main {
         System.out.println("HelloWorld5");
         System.out.println("HelloWorld6");
         System.out.println("HelloWorld7"); //added
-        System.out.println("THis is a test"); //added
+        System.out.println("THis is a test1"); //added
     }
 }
