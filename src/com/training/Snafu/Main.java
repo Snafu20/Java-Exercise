bd94245121233165717240031ae844d9e104c467
Aruna <aruna.snafu@gmail.com>
2021-02-14 13:08:38 +0000

    added 6 on Branch

--- src/com/training/Snafu/Main.java
+++ src/com/training/Snafu/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("HelloWorld1");
         System.out.println("HelloWorld2");
         System.out.println("HelloWorld5");
+        System.out.println("HelloWorld6");
     }
 }
