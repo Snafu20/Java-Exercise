f2a55f5a672cb40e638fc300afdb7c7fbc309ee8
Aruna <aruna.snafu@gmail.com>
2021-02-14 13:34:38 +0000

    added 6 on Branch

--- src/com/training/Snafu/Main.java
+++ src/com/training/Snafu/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //this is a comment
         System.out.println("HelloWorld1");
         System.out.println("HelloWorld2");
+        System.out.println("HelloWorld3");
         System.out.println("HelloWorld4");
     }
 }
